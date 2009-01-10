663836b346caddf44ac0f06ca0e6c793bb6cbcec
Adolfo Morales <adolfomorales@adolfo-moraless-macbook.local>
2009-01-09 21:41:51 -0600

    Generando el modelo para la tabla estados

---
+++ application/models/EstadoModel.php
@@ -0,0 +1,34 @@
+<?PHP
+/* vim: set ts=3 sw=3 sts=3 et si ai: */
+
+/**
+ * FiestaKit :: Página para promocionar todo lo necesario para fiestas 
+ * 
+ * @author       Adolfo Morales  <adolfo.morales@gmail.com> 
+ * @package      FiestaKit
+ * @copyright    Copyright 2009 MashedCode Co.	
+ * @filesource   
+ * 
+ */
+
+//
+//	requires
+/**
+ * EstadoModel.php :: --here short description--
+ * 
+ * --long description--
+ * 
+ * @author       Adolfo Morales  <adolfo.morales@gmail.com> 
+ * @package      FiestaKit
+ * @subpackage   
+ * @category     
+ * @version      $Id: vimrc.local 39 2008-05-10 06:44:21Z andresaquino $
+ * 
+ */
+
+class ServerModel extends Zend_Db_Table_Abstract
+{
+   protected $_name = 'estado';
+
+}
+
